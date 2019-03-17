0445c06e61475500579d0ce1390df5b3e107cda6
mentos <mrbor.biuro@gmail.com>
2019-03-17 21:44:50 +0100

    Cwiczenia od 1-3 + Fibonacci

--- Bortkiewicz-walidator.py
+++ Bortkiewicz-walidator.py
@@ -8,11 +8,11 @@ if(len(pes) == 11):
     age = pes[0] + pes[1]
     month = pes[2] + pes[3]
     if int(month) > 12 or int(month) < 0:
-        print("Pesel niepoprawny")
+        print("Pesel niepoprawny - miesiac")
         exit()
     day = pes[4] + pes[5]
     if int(day) > 31 or int(day) < 0:
-        print("Pesel niepoprawny")
+        print("Pesel niepoprawny - dzien")
         exit()
     print("Data urodzenia: ",age,"/",month,"/",day)
     sex = pes[6] + pes[7] + pes[8] + pes[9]
@@ -25,10 +25,11 @@ if(len(pes) == 11):
     sum = sum + int(pes[2])*7+ int(pes[6])*7
     sum = sum + int(pes[3])*9+ int(pes[7])*9
     sum = sum % 10
+    sum = 10 - sum
     if int(pes[10]) == int(sum):
         print("Pesel poprawny")
     else:
-        print("Pesel niepoprawny")
+        print("Pesel niepoprawny - suma kontrolna", sum)
 
 
 else:
